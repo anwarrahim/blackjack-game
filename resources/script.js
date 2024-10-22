895de3a8209f15bb937d8c1bdcc44801060bb452
Anwar Rahim <anwargithub@gmail.com>
2024-10-22 22:31:45 +0500

    button working

--- resources/script.js
+++ resources/script.js
@@ -5,23 +5,25 @@ let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let message = ""
+let message = " "
 
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
 
 let messageEl = document.getElementById("message-el")
-function startGame(){
+console.log(messageEl)
+
+function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack! "
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game! "
         isAlive = false
     }
-
-    message.messageEl()
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
 
