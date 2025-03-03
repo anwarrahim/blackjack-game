10b399ad2f559621c9ac4bc560c3e6b458d32974
Anwar Rahim <anwargithub@gmail.com>
2025-03-04 03:18:03 +0500

    Refactor game button and implement card shuffling functionality

--- resources/script.js
+++ resources/script.js
@@ -1,87 +1,19 @@
-
-// creating object
-let player = {
-    name : "Anwar",
-    chips: 34
-}
-
-
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = " "
+const gameBtn = document.getElementById('game-btn')
 
 
 
-let messageEl = document.getElementById("message-el")
-console.log(messageEl)
-// sum the value of card
-
-let sumEl = document.getElementById("sum-el")
-
-// card values
-
-let cardEl = document.querySelector(".card-el")
-let playerEl = document.getElementById("player-el")
-
-
-function startGame(){
-    isAlive = true
-    let firstCard = getRandomNumber()
-    let secondCard = getRandomNumber()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    renderGame()
-}
-
-//..................................
-//
-// Get Random Number.................................
-
-function getRandomNumber(){
-   let randomNumber= Math.floor(Math.random() * 13) +1
-    if (randomNumber > 10){
-        return 10
-    }
-    else if (randomNumber === 1){
-        return 11
-    }
-    else {
-        return randomNumber
-    }
-}
-
-
-function renderGame() {
-    cardEl.textContent = "Card: "
-    for(let i =0; i<cards.length; i++){
-        cardEl.textContent += cards[i] +" "
-    }
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? "
-        isAlive = true
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! "
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game! "
-        isAlive = false
-    }
-    // 2. Display the message in the messageEl using messageEl.textContent
-    messageEl.textContent = message
-    playerEl.textContent = player.name +": $"+ player.chips
-}
-
-
-function newCard(){
-   if(isAlive === true || hasBlackJack ===true){
-       let card = getRandomNumber()
-       cards.push(card)
-       sum +=card
-       renderGame()
-   }
-}
-
-
+gameBtn.addEventListener('click',()=>{
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then( response =>{
+        if(!response.ok){
+            throw new Error("Network response was not ok", response.statusText)
+        }
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+    })
+    .catch(error =>{
+        console.error('There has been a problem with your fetch operation', error)
+    })
+})
